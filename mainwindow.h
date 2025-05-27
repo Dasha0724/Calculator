150a1f8563c041ce6f439cfba7356742d98714da
Dasha0724 <dasha.buslovich.07@gmail.com>
2025-05-27 19:45:28 +0300

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_opClearOne_pressed();
+    void digit_pressed();
+    void on_opClear_pressed();
+    void on_opEqual_pressed();
+    void binary_operation();
+    void add_brackets();
+    void unary_operation();
+    void Plus_Minus();
+    void change_sign();
+    void dot_pressed();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
